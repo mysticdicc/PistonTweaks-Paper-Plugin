8436375ca959d4e499a943e7f672d216565031c3
mysticdicc <richardfosterwork@gmail.com>
2026-04-17 22:53:50 +0100

    added logic to deal with sticky blocks

--- src/main/java/dank/net/pistonTweaks/PistonTweaks.java
+++ src/main/java/dank/net/pistonTweaks/PistonTweaks.java
@@ -1,6 +1,10 @@
 package dank.net.pistonTweaks;
 
+import io.papermc.paper.command.brigadier.BasicCommand;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -15,19 +19,55 @@ import org.bukkit.event.block.BlockRedstoneEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.block.data.type.TechnicalPiston;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public final class PistonTweaks extends JavaPlugin implements Listener {
-    private int stickyPistonStrength = 3;
-    private int pistonStrength = 3;
+    private int stickyPistonPullStrength = 3;
+    private int stickyPistonPushStrength = 3;
+    private int pistonPushStrength = 3;
+    private List<Material> stickyBlocks = List.of(
+            Material.SLIME_BLOCK,
+            Material.HONEY_BLOCK
+    );
+
+    List<BlockFace> faces = new ArrayList<>(List.of(
+            BlockFace.UP,
+            BlockFace.DOWN,
+            BlockFace.NORTH,
+            BlockFace.SOUTH,
+            BlockFace.EAST,
+            BlockFace.WEST
+    ));
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         loadConfig();
         getServer().getPluginManager().registerEvents(this, this);
+        registerCommands();
         getLogger().info("Enabled");
+    }
+
+    private void registerCommands() {
+        registerCommand("reload", new BasicCommand() {
+            @Override
+            public void execute(CommandSourceStack commandSourceStack, String[] args) {
+                loadConfig();
+                getLogger().info("Configuration reloaded.");
+
+                var player = commandSourceStack.getExecutor();
+                if (player == null) return;
+                player.sendMessage("Configuration reloaded.");
+                player.sendMessage("sticky-piston-push-strength: " + stickyPistonPushStrength);
+                player.sendMessage("sticky-piston-pull-strength: " + stickyPistonPullStrength);
+                player.sendMessage("piston-push-strength: " + pistonPushStrength);
+                player.sendMessage("sticky-blocks: ");
+
+                for (var material : stickyBlocks) {
+                    player.sendMessage(material.name());
+                }
+            }
+        });
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -41,6 +81,8 @@ public final class PistonTweaks extends JavaPlugin implements Listener {
 
         if (blocks == null) return;
         getLogger().info("[EXTEND] push blocks=" + blocks.size());
+        if (piston.getType() == Material.STICKY_PISTON && blocks.size() > stickyPistonPushStrength) return;
+        if (piston.getType() == Material.PISTON && blocks.size() > pistonPushStrength) return;
 
         Bukkit.getScheduler().runTask(this, () -> {
             for (int i = blocks.size() - 1; i >= 0; i--) {
@@ -68,37 +110,84 @@ public final class PistonTweaks extends JavaPlugin implements Listener {
     }
 
     private List<Block> getRelevantPistonBlocksPush(Block piston, BlockFace face) {
-        var list = new ArrayList<Block>();
         var current = piston.getRelative(face);
+        if (current.getType().isAir()) return null;
         int strength = 1;
 
         if (piston.getType() == Material.STICKY_PISTON) {
-            strength = stickyPistonStrength;
+            strength = stickyPistonPushStrength;
         }
         else if (piston.getType() == Material.PISTON) {
-            strength = pistonStrength;
+            strength = pistonPushStrength;
         }
 
-        for (int i = 0; i < strength; i++) {
-            if (current.getType().isAir()) {
-                return list;
+        Set<Block> toMove = new HashSet<>();
+        ArrayDeque<Block> queue = new ArrayDeque<>();
+        queue.add(current);
+
+        while (!queue.isEmpty()) {
+            Block block = queue.removeFirst();
+
+            if (!toMove.add(block)) continue;
+            if (toMove.size() > strength) return null;
+
+            if (stickyBlocks.contains(block.getType())) {
+                Set<Block> visited = new HashSet<>();
+                Set<Block> results = new HashSet<>();
+
+                var stickyBlocks = handleStickyBlocks(block, visited, results);
+                if (!stickyBlocks.isEmpty()) toMove.addAll(stickyBlocks);
+                if (toMove.size() > strength) return null;
             }
 
-            list.add(current);
-            current = current.getRelative(face);
+            var forward = block.getRelative(face);
+            if (!forward.getType().isAir()) queue.add(forward);
         }
 
-        if (!current.getType().isAir()) {
-            return null;
+        return new ArrayList<Block>(toMove);
+    }
+
+    private boolean canStick(Block block) {
+        List<Material> cantStick = List.of(
+                Material.STICKY_PISTON,
+                Material.PISTON
+        );
+
+        var type = block.getType();
+        if (type.isAir()) return false;
+        if (cantStick.contains(type)) return false;
+
+        return true;
+    }
+
+    private List<Block> handleStickyBlocks(Block source, Set<Block> visited, Set<Block> result) {
+        if (!visited.add(source)) {
+            return new ArrayList<>(result);
         }
 
-        return list;
+        getLogger().info("[STICKY] source=" + source.getType());
+        if (canStick(source)) result.add(source);
+
+        for (BlockFace face : faces) {
+            Block neighbour = source.getRelative(face);
+
+            if (neighbour.getType().isAir()) continue;
+            if (neighbour.equals(source)) continue;
+
+            if (stickyBlocks.contains(neighbour.getType())) {
+                handleStickyBlocks(neighbour, visited, result);
+            } else {
+                if (canStick(neighbour)) result.add(neighbour);
+            }
+        }
+
+        return new ArrayList<>(result);
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onPistonRetract(BlockPistonRetractEvent event) {
         if (!event.isSticky()) return;
-        if(event.getBlock().isEmpty()) return;
+        if(event.getBlocks().isEmpty()) return;
 
         var piston = event.getBlock();
         getLogger().info("[RETRACT] block=" + piston.getType());
@@ -107,6 +196,7 @@ public final class PistonTweaks extends JavaPlugin implements Listener {
         List<Block> blocks = getRelevantPistolBlocksPull(piston, facing);
         getLogger().info("[RETRACT] pull blocks=" + blocks.size() + " facing=" + facing);
 
+        if (blocks.size() > stickyPistonPullStrength) return;
         event.setCancelled(true);
 
         Bukkit.getScheduler().runTask(this, () -> {
@@ -128,20 +218,33 @@ public final class PistonTweaks extends JavaPlugin implements Listener {
     }
 
     private List<Block> getRelevantPistolBlocksPull(Block piston, BlockFace face) {
-        var list = new ArrayList<Block>();
         var current = piston.getRelative(face, 2);
-        int strength = Math.max(1, stickyPistonStrength);
+        int strength = Math.max(1, stickyPistonPullStrength);
 
-        for (int i = 0; i < strength; i++) {
-            if (current.getType().isAir()) {
-                return list;
+        Set<Block> toMove = new HashSet<>();
+        ArrayDeque<Block> queue = new ArrayDeque<>();
+        queue.add(current);
+
+        while (!queue.isEmpty()) {
+            Block block = queue.removeFirst();
+
+            if (!toMove.add(block)) continue;
+            if (toMove.size() > strength) return null;
+
+            if (stickyBlocks.contains(block.getType())) {
+                Set<Block> visited = new HashSet<>();
+                Set<Block> results = new HashSet<>();
+
+                var stickyBlocks = handleStickyBlocks(block, visited, results);
+                if (!stickyBlocks.isEmpty()) toMove.addAll(stickyBlocks);
+                if (toMove.size() > strength) return null;
             }
 
-            list.add(current);
-            current = current.getRelative(face);
+            var forward = block.getRelative(face);
+            if (!forward.getType().isAir()) queue.add(forward);
         }
 
-        return list;
+        return new ArrayList<Block>(toMove);
     }
 
     @Override
@@ -150,8 +253,22 @@ public final class PistonTweaks extends JavaPlugin implements Listener {
     }
 
     private void loadConfig() {
+        reloadConfig();
         var config = getConfig();
-        stickyPistonStrength = config.getInt("sticky-piston-strength");
-        pistonStrength = config.getInt("piston-strength");
+        stickyPistonPullStrength = config.getInt("sticky-piston-pull-strength");
+        stickyPistonPushStrength = config.getInt("sticky-piston-push-strength");
+        pistonPushStrength = config.getInt("piston-push-strength");
+
+        stickyBlocks = new ArrayList<>();
+        var blocks = config.getStringList("sticky-blocks");
+        if (blocks.isEmpty()) return;
+
+        List<Material> materials = blocks.stream()
+                .map(String::toUpperCase)
+                .map(Material::valueOf)
+                .toList();
+
+        if (materials.isEmpty()) return;
+        stickyBlocks.addAll(materials);
     }
 }
